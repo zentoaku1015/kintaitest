32eac3286eaa58ff00b76b459fee4577ba922e25
zentoaku1015 <zentoaku.create@gmail.con>
2026-01-06 00:14:43 +0900

    fix: force dynamic rendering for login page to fetch stores

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { getStores } from './actions'
 import { LoginForm } from '@/components/login-form'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const stores = await getStores()
 
